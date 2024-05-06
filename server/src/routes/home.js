c9a3f9a48a265eaa77519646f409d37c97b7f004
estebanmz <juanestebanmunoz.jems@gmail.com>
2024-05-05 23:19:28 -0500

    feat: Made some improvements; Wrap logic in a try-catch

--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -4,35 +4,36 @@ const router = Router();
 const { HOME_BASE_URL } = require("../globals.js");
 
 router.get("/api/items", async (req, res) => {
-  const { q } = req.query;
-  const response = await axios.get(`${HOME_BASE_URL}${q}`);
-  const data = await response.data;
-  console.log(data.results.slice);
-  res.status(response.status).send({
-    author: {
-      name: "Juan Esteban",
-      lastname: "Munoz Salazar",
-    },
-    categories: response.data.available_filters
-      .find((filter) => filter.id == "category")
-      .values.map((categories) => {
-        return categories.name;
+  try {
+    const { q } = req.query;
+    const response = await axios.get(`${HOME_BASE_URL}${q}`);
+    const { data, status } = await response;
+    res.status(status).send({
+      author: {
+        name: "Juan Esteban",
+        lastname: "Munoz Salazar",
+      },
+      categories: data.available_filters
+        .find((filter) => filter.id == "category")
+        .values.map((categories) => categories.name),
+      items: data.results.slice(0, 4).map((item) => {
+        return {
+          id: item.id,
+          title: item.title,
+          price: {
+            currency: item.currency_id,
+            amount: item.price.toString().split(".")[0],
+            decimals: item.price.toString().split(".")[1],
+          },
+          picture: item.thumbnail,
+          condition: item.condition,
+          free_shipping: item.shipping.free_shipping,
+        };
       }),
-    items: response.data.results.slice(0, 4).map((item) => {
-      return {
-        id: item.id,
-        title: item.title,
-        price: {
-          currency: item.currency_id,
-          amount: item.price.toString().split(".")[0],
-          decimals: item.price.toString().split(".")[1],
-        },
-        picture: item.thumbnail,
-        condition: item.condition,
-        free_shipping: item.shipping.free_shipping,
-      };
-    }),
-  });
+    });
+  } catch (error) {
+    throw new Error("Error fetching search value" + error);
+  }
 });
 
 module.exports = router;
