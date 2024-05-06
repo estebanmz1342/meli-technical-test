55c30156e2f362a9299c82910d6f10014c8555e7
estebanmz <juanestebanmunoz.jems@gmail.com>
2024-05-05 22:24:34 -0500

    feat: Create globals

---
+++ server/src/globals.js
@@ -0,0 +1,4 @@
+const HOME_BASE_URL = "https://api.mercadolibre.com/sites/MLA/search?q=";
+const ITEMS_BASE_URL = "https://api.mercadolibre.com/items/";
+
+module.exports = { HOME_BASE_URL, ITEMS_BASE_URL };
